447e907d21523c199cbc9d146852c32f69cea1ab
Thanh Nguyen Quang <thanhng948@gmail.com>
2018-01-08 08:55:42 +0700

    Initial Commit

---
+++ Test-git/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Test-git
+//
+//  Created by thanh on 2018. 1. 8..
+//  Copyright © 2018년 DIA Developer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
